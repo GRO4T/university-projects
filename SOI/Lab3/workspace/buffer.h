83a01958edadaa6cb604cd47715fec6f7af55fe6
GRO4T <damian.kolaska99@gmail.com>
2020-04-29 23:38:53 +0200

    A1 and B1

--- SOI/Lab3/workspace/buffer.h
+++ SOI/Lab3/workspace/buffer.h
@@ -9,7 +9,10 @@
 #include <semaphore.h>
 #include <limits.h>
 
-#define M 10
+#define M 100
+#define NUMBER_OF_PROC 2
+#define A1 0
+#define B1 1
 
 typedef struct Node{
     int data;
@@ -22,6 +25,13 @@ typedef struct Buffer{
     int size;
     int headId;
     int tailId;
+
+    sem_t even_sem;
+    int number_of_even;
+    
+    sem_t condition_sem[NUMBER_OF_PROC];
+    sem_t proc_sem[NUMBER_OF_PROC];
+    int next_even_mod_100;
 } Buffer;
 
 Buffer * Buffer__bind(){
@@ -41,6 +51,17 @@ void Buffer__init(Buffer * self){
     sem_init(&self->semaphore, 1, 1);
     self->headId = self->size = 0;
     self->tailId = M - 1;
+
+    sem_init(&self->even_sem, 1, 1);
+    self->number_of_even = 0;
+
+    for (int i = 0; i < NUMBER_OF_PROC; ++i){
+        sem_init(&self->proc_sem[i], 1, 1);
+        sem_init(&self->condition_sem[i], 1, 1);
+    }
+    self->next_even_mod_100 = -2;
+
+    sem_wait(&self->condition_sem[B1]);
 }
 
 int Buffer__is_full(Buffer * self){
@@ -57,6 +78,15 @@ void Buffer__put(Buffer * self, int value){
         self->tailId = (self->tailId + 1) % M;
         self->data[self->tailId] = value;
         self->size++;
+
+        if (value % 2 == 0){
+            self->number_of_even++;
+            if (self->number_of_even == 10)
+                sem_wait(&self->condition_sem[A1]);
+        }
+
+        if (self->size == 3)
+            sem_post(&self->condition_sem[B1]);
     }
     sem_post(&self->semaphore);
 }
@@ -70,6 +100,15 @@ int Buffer__pop(Buffer * self){
         temp = self->data[self->headId];
         self->headId = (self->headId + 1) % M;
         self->size--;
+
+        if (temp % 2 == 0){
+            self->number_of_even--;
+            if (self->number_of_even == 9)
+                sem_post(&self->condition_sem[A1]);
+        }
+
+        if (self->size == 2)
+            sem_wait(&self->condition_sem[B1]);
     }
     sem_post(&self->semaphore);
     return temp;
@@ -84,9 +123,25 @@ int Buffer__peek(Buffer * self){
         temp = self->data[self->headId];
     }
     sem_post(&self->semaphore);
-    return temp;
+    return temp; }
+
+int Buffer__less_than_10_even(Buffer * self){
+    sem_wait(&self->condition_sem[A1]);
+    sem_post(&self->condition_sem[A1]);
+    return 1;
 }
 
+int Buffer__next_even_mod_100(Buffer * self){
+    sem_wait(&self->proc_sem[A1]);
+    self->next_even_mod_100 = (self->next_even_mod_100 + 2) % 100;
+    sem_post(&self->proc_sem[A1]);
+    return self->next_even_mod_100;
+}
 
+int Buffer__at_least_3(Buffer * self){
+    sem_wait(&self->condition_sem[B1]);
+    sem_post(&self->condition_sem[B1]);
+    return 1;
+}
 
 #endif
--- SOI/Lab3/workspace/main.c
+++ SOI/Lab3/workspace/main.c
@@ -4,26 +4,58 @@
 
 #include "buffer.h"
 
-int main(){
-    Buffer * buf = Buffer__bind();
-    Buffer__init(buf);
-    
-    pid_t child_pid = fork();
-    if (child_pid != 0)
-        child_pid = fork();
-    pid_t my_pid = getpid();
-    if (child_pid == (0)){
-        for (int i = 0; i < 2; ++i){
-            printf("%d put\n", my_pid);
-            Buffer__put(buf, my_pid);
-            int val = Buffer__pop(buf);
-            printf("%d get, value = %d\n", my_pid, val);
-        }
+int * next_a1_number;
+
+void spawn_process(void (*process_function)(Buffer * buf), Buffer * buf){
+    pid_t pid = fork();
+    if (pid == 0){
+        process_function(buf);
         exit(0);
     }
-    else{
-        while (wait(NULL) > 0);
-    }
-    
+}
 
+void a1_process(Buffer * buf){
+        for (int i = 0; i < 4; ++i){
+            int val;
+            if (Buffer__less_than_10_even(buf)){
+                val = Buffer__next_even_mod_100(buf);
+                Buffer__put(buf, val);
+            }
+            printf("A1 puts %d\n", val);
+        }
+}
+
+void b1_process(Buffer * buf){
+    for (int i = 0; i < 10; ++i){
+        if (Buffer__at_least_3(buf)){
+            if (Buffer__peek(buf) % 2 == 0)
+                printf("B1 pops %d\n", Buffer__pop(buf));
+        }
+    }
+}
+
+int * create_shared_int(){
+    static int shm_id = 0;
+    if (shm_id == 0)
+        shm_id = shmget(IPC_PRIVATE, sizeof(int), SHM_W | SHM_R);
+
+    if (shm_id <= 0){
+        printf("shmget failed...\n");
+        abort();
+    }
+
+    return (int *) shmat(shm_id, NULL, 0);
+}
+
+int main(){
+    //initialize shared variables
+    next_a1_number = create_shared_int();
+
+    Buffer * buf = Buffer__bind();
+    Buffer__init(buf);
+
+    spawn_process(a1_process, buf);
+    spawn_process(b1_process, buf);
+
+    while (wait(NULL) > 0);
 }
