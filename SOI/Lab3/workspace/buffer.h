02ceac184e1dd2f76f1b75eb00a71d03f5715619
GRO4T <damian.kolaska99@gmail.com>
2020-04-29 22:23:30 +0200

    FIFO buffer

--- SOI/Lab3/workspace/buffer.h
+++ SOI/Lab3/workspace/buffer.h
@@ -7,29 +7,24 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 #include <semaphore.h>
+#include <limits.h>
+
+#define M 10
+
+typedef struct Node{
+    int data;
+    struct Node * next;
+} Node;
 
 typedef struct Buffer{
     sem_t semaphore;
-    int data;
-
+    int data[M];
+    int size;
+    int headId;
+    int tailId;
 } Buffer;
-/*  
-sem_t * bind_sem(){
-    static int shm_id = 0;
-    if (shm_id == 0)
-        shm_id = shmget(IPC_PRIVATE, sizeof(sem_t), SHM_W | SHM_R);
-
-    if (shm_id <= 0){
-        printf("shmget failed...\n");
-        abort();
-    }
-
-    return (sem_t *) shmat(shm_id, NULL, 0);
-}
-*/
 
 Buffer * Buffer__bind(){
-    //semaphore = bind_sem();
     static int shm_id = 0;
     if (shm_id == 0)
         shm_id = shmget(IPC_PRIVATE, sizeof(Buffer), SHM_W | SHM_R);
@@ -44,18 +39,50 @@ Buffer * Buffer__bind(){
 
 void Buffer__init(Buffer * self){
     sem_init(&self->semaphore, 1, 1);
+    self->headId = self->size = 0;
+    self->tailId = M - 1;
+}
+
+int Buffer__is_full(Buffer * self){
+    return (self->size == M);
+}
+
+int Buffer__is_empty(Buffer * self){
+    return (self->size == 0);
 }
 
 void Buffer__put(Buffer * self, int value){
     sem_wait(&self->semaphore);
-    self->data = value;
+    if (Buffer__is_full(self) == 0){
+        self->tailId = (self->tailId + 1) % M;
+        self->data[self->tailId] = value;
+        self->size++;
+    }
     sem_post(&self->semaphore);
 }
 
-int Buffer__get(Buffer * self){
+int Buffer__pop(Buffer * self){
     int temp;
     sem_wait(&self->semaphore);
-    temp = self->data;
+    if (Buffer__is_empty(self))
+        temp = INT_MIN;
+    else{
+        temp = self->data[self->headId];
+        self->headId = (self->headId + 1) % M;
+        self->size--;
+    }
+    sem_post(&self->semaphore);
+    return temp;
+}
+
+int Buffer__peek(Buffer * self){
+    int temp;
+    sem_wait(&self->semaphore);
+    if (Buffer__is_empty(self))
+        temp = INT_MIN;
+    else{
+        temp = self->data[self->headId];
+    }
     sem_post(&self->semaphore);
     return temp;
 }
--- SOI/Lab3/workspace/main.c
+++ SOI/Lab3/workspace/main.c
@@ -4,19 +4,6 @@
 
 #include "buffer.h"
 
-sem_t * bind_sem(){
-    static int shm_id = 0;
-    if (shm_id == 0)
-        shm_id = shmget(IPC_PRIVATE, sizeof(sem_t), SHM_W | SHM_R);
-
-    if (shm_id <= 0){
-        printf("shmget failed...\n");
-        abort();
-    }
-
-    return (sem_t *) shmat(shm_id, NULL, 0);
-}
-
 int main(){
     Buffer * buf = Buffer__bind();
     Buffer__init(buf);
@@ -29,23 +16,12 @@ int main(){
         for (int i = 0; i < 2; ++i){
             printf("%d put\n", my_pid);
             Buffer__put(buf, my_pid);
-            int val = Buffer__get(buf);
+            int val = Buffer__pop(buf);
             printf("%d get, value = %d\n", my_pid, val);
         }
         exit(0);
     }
-    else{/*
-        for (int i = 0; i < 2; ++i){
-            printf("%d lock mutex\n", child_pid);
-            sem_wait(semaphore);
-            int semval = 0;
-            sem_getvalue(semaphore, &semval);
-            printf("sem = %d\n", semval);
-            printf("unlock mutex\n");
-            sem_post(semaphore);
-            sem_getvalue(semaphore, &semval);
-            printf("sem = %d\n", semval);
-        }*/
+    else{
         while (wait(NULL) > 0);
     }
     
