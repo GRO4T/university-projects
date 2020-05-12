23df5155c2c864128eb3de9ccae9d9c44a9558b5
GRO4T <damian.kolaska99@gmail.com>
2020-05-12 13:24:05 +0200

    [SOI] A1 and B1 threads work

--- SOI/Lab4/workspace/monitor/main.cpp
+++ SOI/Lab4/workspace/monitor/main.cpp
@@ -7,6 +7,11 @@
 #define CONS_COUNT 20  //liczba konsumentow
 #define ELEM_COUNT 1000  //liczba produkowanych elementow
 
+#define A1_COUNT 5
+#define B1_COUNT 5
+
+#define SLEEPTIME 500000
+
 //Bufor jednoelementowy
 class Buffer: public Monitor
 {
@@ -81,6 +86,14 @@ public:
 
     }
 
+    int getSize(){
+        int temp;
+        enter();
+        temp = items.size();
+        leave();
+        return temp;
+    }
+
     void print(){
         std::queue<int> temp = items;
         std::cout << temp.size();
@@ -100,9 +113,62 @@ protected:
 	Condition emptyCond, fullCond;
 };
 
+class ThreadMonitor : public Monitor{
+    public:
+        ThreadMonitor(Buffer & buf) : buffer(buf), nextEven(-2), nextOdd(-1){}
+
+        int putEven(int nr){
+            enter();
+            int temp;
+            if (buffer.getSize() >= 10)
+                wait(a1Cond);
+            nextEven = (nextEven + 2) % 100;
+            temp = nextEven;
+            buffer.put(nextEven);
+
+            printf("A1 producer nr:%d %d\t", nr, nextEven);
+            buffer.print();
+            
+            if (buffer.getSize() >= 3 && buffer.peek() % 2 == 0)
+                signal(b1Cond);
+
+            leave();
+            return temp;
+        }
+
+        int getEven(){
+            enter();
+            int temp;
+            if (buffer.getSize() < 3 || buffer.peek() % 2 != 0)
+                wait(b1Cond);
+            temp = buffer.get();
+
+            if (buffer.getSize() < 10)
+                signal(a1Cond);
+
+            leave();
+            return temp;
+        }
+        void checkLess10(){
+
+        }
+
+        void checkLeast3(){
+
+        }
+
+        void checkLeast7(){
+
+        }
+    protected:
+        Condition a1Cond, a2Cond, b1Cond, b2Cond;
+        Buffer & buffer;
+        int nextEven;
+        int nextOdd;
+};
 
 Buffer buffer;
-
+ThreadMonitor threadMonitor(buffer);
 
 void * producent( void * )
 {
@@ -111,7 +177,6 @@ void * producent( void * )
 	for( int i = 1; i <= ELEM_COUNT; ++ i )
 	{
 		buffer.put( i );
-		//sleep(1);
 	}
 
 	//aby zakonczycz konsumentow
@@ -120,8 +185,6 @@ void * producent( void * )
 
 	return 0;
 }
-
-
 
  void * konsument( void * nr )
 {
@@ -140,21 +203,46 @@ void * producent( void * )
 	return 0;
 }
 
+void * a1Producer(void * nr){
+	for( int i = 1; i <= ELEM_COUNT; ++ i )
+	{
+        usleep(SLEEPTIME);
+		threadMonitor.putEven((intptr_t)nr);
+	}
+
+    return 0;
+}
+
+void * b1Consumer(void * nr){
+    int val;
+    while (true){
+        usleep(SLEEPTIME);
+        val = threadMonitor.getEven();
+        printf("B1 Consumer nr:%d %d\n", nr, val);
+    }
+
+    return 0;
+}
+
 int main()
 {
 	int i;
-	pthread_t tid[CONS_COUNT+1];
+	pthread_t tid[A1_COUNT + B1_COUNT];
 
 	//utworz watek producenta
-	pthread_create(&(tid[0]), NULL, konsument, 0 );
+	//pthread_create(&(tid[0]), NULL, konsument, 0 );
 
-	//utworz watki konsumentow
-	for ( i = 1; i <= CONS_COUNT; i++)
-		pthread_create(&(tid[i]), NULL, producent, (void*)(intptr_t)i );
+	//utworz watki producentów A1
+	for ( i = 0; i < A1_COUNT; i++)
+		pthread_create(&(tid[i]), NULL, a1Producer, (void*)(intptr_t)i );
+
+    //utwórz wątki konsumentów B1
+	for ( i = A1_COUNT; i < A1_COUNT + B1_COUNT; i++)
+		pthread_create(&(tid[i]), NULL, b1Consumer, (void*)(intptr_t)(i-A1_COUNT));
 
 
 	//czekaj na zakonczenie watkow
-	for ( i = 0; i < CONS_COUNT + 1; i++)
+	for ( i = 0; i < A1_COUNT; i++)
 	  pthread_join(tid[i],(void **)NULL);
 	return 0;
 }
